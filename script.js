d276fb402336e9cc7c6dd0564d63edb40936352c
AbhayShastry <abhay.s.shastry@gmail.com>
2022-11-19 22:15:55 +0530

    Added About me section and got started on projects section

--- script.js
+++ script.js
@@ -59,23 +59,69 @@ document.body.addEventListener('mouseleave', () => {
 })
 
 //Mai Button
-const mainBtn = document.querySelector(".main-btn")
-let ripple;
-mainBtn.addEventListener("mouseenter", (e) => {
-    const left = e.clientX - e.target.getBoundingClientRect().left;
-    console.log(left);
-    const top = e.clientY - e.target.getBoundingClientRect().top;
-    console.log(top);
+const mainBtns = document.querySelectorAll(".main-btn")
 
-    ripple = document.createElement("div")
-    ripple.classList.add("ripple");
-    ripple.style.left = `${left}px`
-    ripple.style.top = `${top}px`
-    mainBtn.prepend(ripple);
+mainBtns.forEach(btn => {
+    let ripple;
+    btn.addEventListener("mouseenter", (e) => {
+        const left = e.clientX - e.target.getBoundingClientRect().left;
+        console.log(left);
+        const top = e.clientY - e.target.getBoundingClientRect().top;
+        console.log(top);
 
+        ripple = document.createElement("div")
+        ripple.classList.add("ripple");
+        ripple.style.left = `${left}px`
+        ripple.style.top = `${top}px`
+        btn.prepend(ripple);
+
+    })
+
+    btn.addEventListener("mouseleave", () => {
+        btn.removeChild(ripple);
+    })
 })
 
-mainBtn.addEventListener("mouseleave", () => {
-    mainBtn.removeChild(ripple);
-})
+
 //End of Main Button
+//About me text
+const aboutMeText = document.querySelector(".about-me-text")
+const aboutMeTextContent = "I am a Software Developer currently working as a Machine Learning Engineer at Saaragh Technologies,a Machine Learning Startup. I was previously a Software Developer Intern at Intel Corporation. I am constantly willing to learn and understand new technologies";
+Array.from(aboutMeTextContent).forEach((char) => {
+
+    const span = document.createElement("span");
+    span.textContent = char;
+    aboutMeText.appendChild(span);
+
+    span.addEventListener("mouseenter", (e) => {
+        e.target.style.animation = "aboutMeTextAnim 10s Infinite";
+    })
+
+});
+//End of About me text
+//Projects
+const container = document.querySelector(".container");
+const projects = document.querySelectorAll(".project");
+
+projects.forEach(project => {
+    project.addEventListener("mouseenter", () => {
+        project.firstElementChild.style.top = `-${project.firstElementChild.offsetHeight - project.offsetHeight + 20}px`;
+    });
+
+    project.addEventListener('mouseleave', () => {
+
+        project.firstElementChild.style.top = "2rem";
+
+    })
+})
+
+//Github link for projects
+project.addEventListener('click', () => {
+    //const imgWrapper = document.createElement('div');
+    //imgWrapper.className = "project-img-wrapper";
+    //container.appendChild(imgWrapper)
+    console.log("clicked");
+    window.open("www.google.com", "_blank");
+})
+//End of github link for projects
+//End of Projects
